78b86bae4e6ee9f79959a5f0efe3fd25f78e17df
Josewin <josewinjosewin@gmail.com>
2023-02-02 18:28:45 +0530

    Create BinarySearch.java

---
+++ BinarySearchAlgorithm/BinarySearch.java
@@ -0,0 +1,19 @@
+
+public class BinarySearch {
+
+    public int search(int[] nums, int target) {
+        int head = 0;
+        int tail = nums.length-1;
+        
+        if(nums[0]== target )   return 0;
+        
+        while(head<tail){
+            int mid = (head + tail)/2;
+            
+            if(nums[mid]<target)   head = mid + 1;
+            else if (nums[mid]>target)  tail = mid - 1;
+            else if (nums[mid]== target)    return  mid;
+        }
+        return -1;
+    }
+}
