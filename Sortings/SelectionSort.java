aba703c1f8010f8ceaddf56743da3a94a931d513
jozewin <josewinjosewin@gmail.com>
2023-04-23 16:09:33 +0530

    Selection Sort

--- Sortings/SelectionSort.java
+++ Sortings/SelectionSort.java
@@ -4,13 +4,32 @@ import java.util.Arrays;
 
 public class SelectionSort {
     public static void ssSort(int[] arr){
+        /**
+         * ==Ascending Order==
+         * Time complexity = O(n^2)
+         */
+        /**
+         * Setting the outer loop which is used to compare
+         */
         for (int i = 0;i<arr.length-1;i++){
+            /**
+             * Setting minIndex to compare inner loop
+             */
             int minIndex = i;
+            /**
+             * Setting the Inner loop which will be used to compare with outer loop
+             */
             for(int j = i+1;j<arr.length;j++){
+                /**
+                 * Condition operations when found j is smaller than i
+                 */
                 if (arr[minIndex]>arr[j]) {
                     minIndex = j;
                 }
             }
+            /**
+             * Swapping i and j
+             */
             int temp = arr[i];
             arr[i] = arr[minIndex];
             arr[minIndex] = temp;
