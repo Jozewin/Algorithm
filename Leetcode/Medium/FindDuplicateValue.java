4781e21ad0408fb4ef488e511dff37678e81c515
Jozewin <110077664+Jozewin@users.noreply.github.com>
2025-01-10 12:42:30 +0530

    Create FindDuplicateValue.java

---
+++ Leetcode/Medium/FindDuplicateValue.java
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/find-the-duplicate-number/description/
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FIndDuplicateElement {
+    public int findDuplicate(int[] nums) {
+        int i = 0;
+
+        while (i < nums.length) {
+            if (nums[i] != i + 1) {
+                int correct = nums[i] - 1;
+
+                if (nums[i] != nums[correct]) {
+                    swap(nums, i, correct);
+                } else {
+                    return nums[i];
+                }
+            }else{
+                i++;
+            }
+        }
+        return -1;
+
+    }
+
+    void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
